f56cce5ea6bd73545bcc816a68cc140a96aff218
AMR-2223 <albmesrod@alu.edu.gva.es>
2023-01-26 16:38:18 +0100

    punto 10, falta terminar

---
+++ WindowsFormsApp1/Form1_previopaso10.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionBancariaAppNS
+{
+    public partial class GestionBancariaApp : Form
+    {
+        private double saldo;  
+        const int ERR_CANTIDAD_NO_VALIDA = 1;
+        const int ERR_SALDO_INSUFICIENTE = 2;
+
+        public GestionBancariaApp(double saldo = 0)
+        {
+            InitializeComponent();
+            if (saldo > 0)
+                this.saldo = saldo;
+            else
+                this.saldo = 0;
+            txtSaldo.Text = obtenerSaldo().ToString();
+            txtCantidad.Text = "0";
+        }
+
+        public double obtenerSaldo() { return saldo; }
+
+        public int realizarReintegro(double cantidad) 
+        {
+            if (cantidad <= 0)
+                //return ERR_CANTIDAD_NO_VALIDA;
+                throw new ArgumentOutOfRangeException("La cantidad indicada no es válida.");
+            if (saldo < cantidad)
+                //return ERR_SALDO_INSUFICIENTE;
+                throw new ArgumentOutOfRangeException("Saldo Insuficiente");
+            saldo -= cantidad; //AMR2223 corregido error
+            return 0;
+        }
+
+        public int realizarIngreso(double cantidad) {
+            if (cantidad < 0) //AMR2223 corregido error
+                //return ERR_CANTIDAD_NO_VALIDA;
+                throw new ArgumentOutOfRangeException("Cantidad no válida");
+            saldo += cantidad; //AMR2223 corregido error
+            return 0;
+        }
+
+        private void btOperar_Click(object sender, EventArgs e)
+        {
+            double cantidad = Convert.ToDouble(txtCantidad.Text); // Cogemos la cantidad del TextBox y la pasamos a número
+            if (rbReintegro.Checked)
+            {
+                int respuesta = realizarReintegro(cantidad);
+                if (respuesta == ERR_SALDO_INSUFICIENTE)
+                    MessageBox.Show("No se ha podido realizar la operación (¿Saldo insuficiente?)");
+                else
+                if (respuesta == ERR_CANTIDAD_NO_VALIDA)
+                    MessageBox.Show("Cantidad no válida, sólo se admiten cantidades positivas.");
+                else
+                    MessageBox.Show("Transacción realizada.");
+
+            }
+            else {
+                if (realizarIngreso(cantidad) == ERR_CANTIDAD_NO_VALIDA)
+                    MessageBox.Show("Cantidad no válida, sólo se admiten cantidades positivas.");
+                else
+                    MessageBox.Show("Transacción realizada.");
+            }
+           txtSaldo.Text = obtenerSaldo().ToString();
+        }
+    }
+}
---
+++ gestionBancariaTest/gestionBancariaTest.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GestionBancariaAppNS; //AMR2223 este es el nombre del namespace 
+
+namespace gestionBancariaTest
+{
+    [TestClass]
+    public class gestionBancariaTest
+    {
+        [TestMethod]
+        public void prueba1_validarIngreso()
+        {
+            // AMR2223 simulamos ingreso
+            double saldoInicial = 0;
+            double ingreso = -250;
+            double saldoEsperado = -250;
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+            // Método a probar
+            miApp.realizarIngreso(ingreso);
+            Assert.AreEqual(saldoEsperado, miApp.obtenerSaldo(), 0.001, "Se produjo un error al realizar el ingreso, saldo incorrecto.");
+        }
+
+        [TestMethod]
+        public void prueba2_validarIngreso()
+        {
+            // AMR2223 simulamos ingresos varias cantidades
+            double[] saldoInicial = new double[] { 0, 0, 2500, 1000000 };
+            double[] ingreso = new double[] { 0, 750, 2500, 1000000 };
+            double[] saldoEsperado = new double[] { 0, 750, 5000, 2000000 };
+
+            for (int i = 0; i < saldoEsperado.Length; i++)
+            {
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial[i]);
+                // Método a probar
+                miApp.realizarIngreso(ingreso[i]);
+                Assert.AreEqual(saldoEsperado[i], miApp.obtenerSaldo(), 0.001, "Se produjo un error al realizar el ingreso, saldo incorrecto.");
+            }
+        }
+
+        [TestMethod]
+        public void prueba3_validarReintegro()
+        {
+            // AMR2223 simulamos reintegro
+            double saldoInicial = 0;
+            double reintegro = -250;
+            double saldoEsperado = -250;
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+            // Método a probar
+            miApp.realizarReintegro(reintegro);
+            Assert.AreEqual(saldoEsperado, miApp.obtenerSaldo(), 0.001, "Se produjo un error al realizar el reintegro, saldo incorrecto.");
+        }
+
+        [TestMethod]
+        public void prueba4_validarReintegro()
+        {
+            // AMR2223 simulamos reintegros varias cantidades
+            double[] saldoInicial = new double[] { 0, 750, 5000, 2000000 };
+            double[] reintegro = new double[] { 0, 750, 2500, 1000000 };
+            double[] saldoEsperado = new double[] { 0, 0, 2500, 1000000 };
+
+            for (int i = 0; i < saldoEsperado.Length; i++)
+            {
+                GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial[i]);
+                // Método a probar
+                miApp.realizarReintegro(reintegro[i]);
+                Assert.AreEqual(saldoEsperado[i], miApp.obtenerSaldo(), 0.001, "Se produjo un error al realizar el ingreso, saldo incorrecto.");
+            }
+        }
+                
+        [TestMethod]
+        public void prueba5_validarReintegro()
+        {
+            // AMR2223 simulamos reintegro
+            double saldoInicial = 1000;
+            double reintegro = 2000;
+            double saldoEsperado = 0;
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+            // Método a probar
+            miApp.realizarReintegro(reintegro);
+            Assert.AreEqual(saldoEsperado, miApp.obtenerSaldo(), 0.001, "Se produjo un error al realizar el reintegro, saldo incorrecto.");
+        }
+
+        /*[TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void validarReintegroCantidadNoValida()
+        {
+            double saldoInicial = 1000;
+            double reintegro = 1250; // AMR2223
+            double saldoFinal = saldoInicial - reintegro;
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+            miApp.realizarReintegro(reintegro);
+        }*/
+
+        [TestMethod]
+        public void validarReintegroCantidadNoValida() //AMR2223
+        {
+            double saldoInicial = 1000;
+            double reintegro = -250;
+            double saldoFinal = saldoInicial - reintegro;
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+            try
+            {
+                miApp.realizarReintegro(reintegro);
+            }
+            catch (ArgumentOutOfRangeException exception)
+            {
+                // assert
+                StringAssert.Contains(exception.Message, GestionBancariaApp.ERR_CANTIDAD_NO_VALIDA);
+                return;
+            }
+            Assert.Fail("Error. Se debía haber producido una excepción.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void validarIngresoCantidadNoValida()
+        {
+            double saldoInicial = 1000;
+            double ingreso = -250; // AMR2223
+            double saldoFinal = saldoInicial + ingreso;
+            GestionBancariaApp miApp = new GestionBancariaApp(saldoInicial);
+            miApp.realizarIngreso(ingreso);
+        }
+    }
+}
